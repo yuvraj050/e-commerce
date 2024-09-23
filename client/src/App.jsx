c50b806bc532533d092d69e52936eb8c4617f906
yuvraj050 <ys09559@gmail.com>
2024-09-24 02:41:25 +0530

    react router setup and redux setup

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,34 +1,37 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
+import './index.css'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
+    <div className="text-center">
+      <div className="flex justify-center gap-4 mt-8">
         <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
+          <img src={viteLogo} className="w-16 h-16" alt="Vite logo" />
         </a>
         <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+          <img src={reactLogo} className="w-16 h-16 react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1 className="text-5xl font-bold my-8">Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
+        <button
+          className="bg-gray-900 text-white py-2 px-4 rounded hover:bg-gray-700"
+          onClick={() => setCount((count) => count + 1)}
+        >
           count is {count}
         </button>
-        <p>
+        <p className="mt-4">
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
       </div>
-      <p className="read-the-docs">
+      <p className="text-gray-500 mt-8">
         Click on the Vite and React logos to learn more
       </p>
-    </>
+    </div>
   )
 }
 
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,16 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
 import App from './App.jsx'
 import './index.css'
+import { Provider } from 'react-redux'
+import store from './store/store.js'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <BrowserRouter>
+    <Provider store ={store}>
+      <App />
+    </Provider>
+  </BrowserRouter>
+    
+  
 )
