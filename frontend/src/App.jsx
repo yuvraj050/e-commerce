6b87729c84babafba8a57367193f9ae3e75414ec
yuvraj050 <ys09559@gmail.com>
2024-10-30 00:29:23 +0530

    Footer completed

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -10,6 +10,7 @@ import Login from './pages/Login'
 import PlaceOrder from './pages/PlaceOrder'
 import Orders from './pages/Orders'
 import Navbar from './components/Navbar'
+import Footer from './components/Footer'
 
 const App = () => {
   return (
@@ -27,6 +28,7 @@ const App = () => {
         <Route path='/orders' element={<Orders/>} />
 
       </Routes>
+      <Footer/>
 
     </div>
   )
