79f8137a557e06580e2ed4b58f382645d36cba62
yuvraj050 <ys09559@gmail.com>
2024-10-31 17:49:03 +0530

    Admin panel added

--- backend/config/cloudinary.js
+++ backend/config/cloudinary.js
@@ -2,7 +2,7 @@ import {v2 as cloudinary } from "cloudinary"
 
 
 const connectCloudinary = async () => {
-    cloudinary.congig({
+    cloudinary.config({
         cloud_name: process.env.CLOUDINARY_NAME,
         api_key: process.env.CLOUDINARY_API_KEY,
         api_secret: process.env.CLOUDINARY_SECRET_KEY
--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ import 'dotenv/config'
 import connectDB from './config/mongodb.js'
 import connectCloudinary from './config/cloudinary.js'
 import userRouter from './routes/userRoute.js'
+import productRouter from './routes/productRoute.js'
 
 
 //App config
@@ -18,6 +19,7 @@ app.use(cors())
 
 // api endpoints
 app.use('/api/user', userRouter)
+app.use('/api/product', productRouter)
 
 app.get('/',(req,res)=>{
     res.send("API Working")
